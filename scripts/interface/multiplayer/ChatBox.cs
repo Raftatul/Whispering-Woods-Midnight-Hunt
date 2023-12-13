b951ea8b40705ee86b6ff52e979c910ea430275d
Angel-2180 <a.gimer@student.isartdigital.com>
2023-12-13 02:20:13 -0500

    Add ChatBox.cs for multiplayer chat functionality

---
+++ scripts/interface/multiplayer/ChatBox.cs
@@ -0,0 +1,54 @@
+using Godot;
+using Steamworks.Data;
+using System;
+using System.Collections.Generic;
+
+public partial class ChatBox : Control
+{
+	[Export]
+	public LineEdit ChatInput { get; set; }
+
+	[Export]
+	public Button SendButton { get; set; }
+
+	[Export]
+	public RichTextLabel ChatHistory { get; set; }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		DataParser.OnChatMessageReceived += OnChatMessageReceivedCallback;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void OnSendButtonPressed()
+	{
+		if (ChatInput.Text.Length > 0)
+		{
+			Dictionary<string, string> data = new Dictionary<string, string>
+            {
+                { "DataType", "ChatMessage" },
+                { "UserName", SteamManager.Instance.PlayerName },
+                { "ChatMessage", ChatInput.Text }
+            };
+			OnChatMessageReceivedCallback(data);
+			string json = OwnJsonParser.Serialize(data);
+			if (SteamManager.Instance.IsHost)
+			{
+				SteamManager.Instance.SendMessageToAll(json);
+			}
+			else
+			{
+				SteamManager.Instance.SteamConnectionManager.Connection.SendMessage(json);
+			}
+		}
+	}
+
+	private void OnChatMessageReceivedCallback(Dictionary<string, string> data)
+	{
+		ChatHistory.Text += data["UserName"] + ": " + data["ChatMessage"] + System.Environment.NewLine;
+	}
+}

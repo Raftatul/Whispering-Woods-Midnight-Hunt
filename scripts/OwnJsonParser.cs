c5cf2ded8cecc48a9bda4f1f5722b99126e05a3b
Angel-2180 <a.gimer@student.isartdigital.com>
2023-12-14 01:48:36 -0500

    Add Godot support to OwnJsonParser

--- scripts/OwnJsonParser.cs
+++ scripts/OwnJsonParser.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Godot;
 
 
 public class OwnJsonParser
@@ -13,7 +15,15 @@ public class OwnJsonParser
         }
         json = json.Remove(json.Length - 1);
         json += "}";
-        return json;
+
+        //godot dictionary
+        Godot.Collections.Dictionary<string, string> godotDictionary = new Godot.Collections.Dictionary<string, string>();
+        foreach (var item in data)
+        {
+            godotDictionary.Add(item.Key, item.Value);
+        }
+        GD.Print(Json.Stringify(godotDictionary));
+        return Json.Stringify(godotDictionary);
     }
 
     public static Dictionary<string, string> Deserialize(string json)
@@ -27,6 +37,18 @@ public class OwnJsonParser
             string[] itemSplit = item.Split(':');
             data.Add(itemSplit[0].Replace("\"", ""), itemSplit[1].Replace("\"", ""));
         }
-        return data;
+
+        Godot.Collections.Dictionary<string, string> godotDictionary;
+        Json res = new Json();
+        res.Parse(json);
+
+        godotDictionary = res.Data.AsGodotDictionary<string, string>();
+        GD.Print(godotDictionary);
+        Dictionary<string, string> godotDictionary2 = new Dictionary<string, string>();
+        foreach (var item in godotDictionary)
+        {
+            godotDictionary2.Add(item.Key, item.Value);
+        }
+        return godotDictionary2;
     }
 }

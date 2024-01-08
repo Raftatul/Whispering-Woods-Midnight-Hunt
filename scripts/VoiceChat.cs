b5119a13377f1a50a6d69531c6775139a2e3208a
RaphaelDaubelcour <daubelcour63@gmail.com>
2024-01-07 20:45:38 -0500

    Fix player voice ouput

--- scripts/VoiceChat.cs
+++ scripts/VoiceChat.cs
@@ -9,13 +9,11 @@ using System.Text;
 public partial class VoiceChat : Node3D
 {
     [Export]
-    private AudioStreamPlayer3D _audioStreamPlayer3D;
-
-    [Export]
     private AudioStreamPlayer _audioStreamRecorder;
 
-    [Export]
     private Timer _sendRecordingTimer;
+
+    private AudioStreamPlayer3D _audioStreamPlayer3D;
 
     private AudioEffectRecord effect;
 
@@ -32,36 +30,39 @@ public partial class VoiceChat : Node3D
         effect = AudioServer.GetBusEffect(idx, 0) as AudioEffectRecord;
         effect.SetRecordingActive(true);
 
+        _sendRecordingTimer = new Timer();
+        _sendRecordingTimer.WaitTime = 0.5f;
         _sendRecordingTimer.Timeout += OnSendRecordingTimerTimeout;
-
-        // DataParser.OnVoiceChat += SendRecordingData;
+        AddChild(_sendRecordingTimer);
+        _sendRecordingTimer.Start();
     }
 
     [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = false, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
-    private void SendRecordingData(byte[] recData)
+    private void SendRecordingData(NodePath audioPlayerPath, byte[] recData)
     {
+        AudioStreamPlayer3D audioStreamPlayer = GetNode<AudioStreamPlayer3D>(audioPlayerPath);
         AudioStreamWav sample = new AudioStreamWav()
         {
             Data = recData,
             Format = AudioStreamWav.FormatEnum.Format16Bits,
             MixRate = (int)AudioServer.GetMixRate() * 2
         };
-        GD.Print("Received recording data");
-        _audioStreamPlayer3D.Stream = sample;
-        _audioStreamPlayer3D.Play();
+        audioStreamPlayer.Stream = sample;
+        audioStreamPlayer.Play();
     }
 
     private void OnSendRecordingTimerTimeout()
     {
         if (Multiplayer.MultiplayerPeer != null)
         {
+            GD.Print("Sending recording data");
+            recording = effect.GetRecording();
+            effect.SetRecordingActive(false);
+            Rpc(nameof(SendRecordingData), _audioStreamPlayer3D.GetPath(), recording.Data);
+            SendRecordingData(_audioStreamPlayer3D.GetPath(), recording.Data);
+            effect.SetRecordingActive(true);
             if (Multiplayer.GetPeers().Length > 0)
             {
-                GD.Print("Sending recording data");
-                recording = effect.GetRecording();
-                effect.SetRecordingActive(false);
-                Rpc(nameof(SendRecordingData), recording.Data);
-                effect.SetRecordingActive(true);
             }
         }
     }

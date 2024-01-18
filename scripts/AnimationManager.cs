e8c32b318968a02c18859d160d54303e1ccdf6a4
RaphaelDaubelcour <daubelcour63@gmail.com>
2024-01-18 00:02:17 -0500

    Add RPC attributes to AnimationManager methods

--- scripts/AnimationManager.cs
+++ scripts/AnimationManager.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 
 public partial class AnimationManager : AnimationTree
 {
+    [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
     public void SetFloat(string path, float value, float lerpSpeed = 0.1f)
     {
         float current = Get("parameters/" + path).AsSingle();
@@ -12,6 +13,7 @@ public partial class AnimationManager : AnimationTree
         Set("parameters/" + path, lerp);
     }
 
+    [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
     public void SetVector2(string path, Vector2 value, float lerpSpeed = 0.1f)
     {
         Vector2 current = Get("parameters/" + path).AsVector2();
@@ -20,6 +22,7 @@ public partial class AnimationManager : AnimationTree
         Set("parameters/" + path, lerp);
     }
 
+    [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
     public void SetBool(string path, bool value)
     {
         Set("parameters/" + path, value);
@@ -29,11 +32,6 @@ public partial class AnimationManager : AnimationTree
     public void RequestTransition(string path, string state)
     {
         Set("parameters/" + path, state);
-    }
-
-    public void RequestOneShot(string path, AnimationNodeOneShot.OneShotRequest request)
-    {
-        Set("parameters/" + path, (int)request);
     }
 
     [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
